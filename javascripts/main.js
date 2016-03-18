dfc26bcd6492673fe1d0c1ea6ae716dd14fdb769
ChangYinShung <changyenshung@gmail.com>
2016-03-19 01:41:01 +0800

    unannounce

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-var app = angular.module('MainApp', ['ui.router', 'ngMaterial', 'ngMdIcons']);
+var app = angular.module('MainApp', ['ui.router', 'ngMaterial', 'ngMdIcons', ]);
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('green');
+
+});
